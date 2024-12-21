7ee250ef06ddbdc7b95f873c5cf508dd3564dafe
Fabrizio Montanez <AndreFabrizioMP10@hotmail.com>
2024-12-21 15:35:33 -0500

    Actualizacion de archivo Java

--- Ventana1.java
+++ Ventana1.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 public class Ventana1 {
     public static void main(String[] args) {
         JFrame vent1 = new JFrame();
+        vent1.setDefaultCloseOperation(vent1.EXIT_ON_CLOSE);
         vent1.setVisible(true);
         vent1.setSize(500, 500);
         vent1.setLocationRelativeTo(null);
