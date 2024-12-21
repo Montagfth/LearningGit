016eec2f503259b2127d01239c793f159d523383
Fabrizio Montanez <AndreFabrizioMP10@hotmail.com>
2024-12-21 08:16:41 -0500

    Primer plano de Java

---
+++ Ventana1.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Ventana1 {
+    public static void main(String[] args) {
+        JFrame vent1 = new JFrame();
+        vent1.setSize(500, 500);
+        vent1.setLocationRelativeTo(null);
+        JPanel panel1 = new JPanel();
+        panel1.setSize(500, 500);
+    }
+    
+}
