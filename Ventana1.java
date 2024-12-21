757e6dc1662ae7a707d45bdd1c278cfcbbfbabe7
Fabrizio Montanez <AndreFabrizioMP10@hotmail.com>
2024-12-21 12:16:34 -0500

    Modificacion Final del Frame

--- Ventana1.java
+++ Ventana1.java
@@ -1,13 +1,12 @@
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Ventana1 {
     public static void main(String[] args) {
         JFrame vent1 = new JFrame();
+        vent1.setVisible(true);
         vent1.setSize(500, 500);
         vent1.setLocationRelativeTo(null);
-        JPanel panel1 = new JPanel();
-        panel1.setSize(500, 500);
+        
     }
     
 }
