5c789185d6ff27674ed15dd416c8b26cb640c457
Fabrizio Montanez <AndreFabrizioMP10@hotmail.com>
2024-12-24 11:33:09 -0500

    Actualizacion e insercion de archivos Java

--- Ventana1.java
+++ Ventana1.java
@@ -3,11 +3,8 @@ import javax.swing.JFrame;
 public class Ventana1 {
     public static void main(String[] args) {
         JFrame vent1 = new JFrame();
-        vent1.setDefaultCloseOperation(vent1.EXIT_ON_CLOSE);
         vent1.setVisible(true);
         vent1.setSize(500, 500);
-        vent1.setLocationRelativeTo(null);
-        
+        vent1.setLocationRelativeTo(null);   
     }
-    
 }
---
+++ Ventana2.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JFrame;
+
+public class Ventana2 {
+    public static void main(String[] args) {
+        //Medidas de la ventana:
+        JFrame ven2 = new JFrame();
+        ven2.setVisible(true);
+        ven2.setSize(500,500);
+        ven2.setLocationRelativeTo(null);
+
+        //Logica de interaccion:
+        
+    }
+}
